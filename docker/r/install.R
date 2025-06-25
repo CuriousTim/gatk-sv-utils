e589138fe47eb01bdf16a4544d427b57767af657
Justin Lim <jlim21@mgh.harvard.edu>
2025-06-24 20:23:32 -0400

    r-docker: update Bioconductor repo

--- docker/r/install.R
+++ docker/r/install.R
@@ -14,7 +14,7 @@ install <- function(x) {
         x[[1]],
         version = x[[2]],
         upgrade = "always",
-        repos = c("https://cloud.r-project.org", "https://bioconductor.org/packages/3.21"),
+        repos = c("https://cloud.r-project.org", "https://bioconductor.org/packages/3.21/bioc"),
         INSTALL_opts = c("--no-docs", "--no-html", "--no-data", "--no-help",
                          "--no-demo", "--without-keep.source"),
         Ncpus = NCPUS

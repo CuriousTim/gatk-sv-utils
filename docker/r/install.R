d718f95d90f42cd323e57105b7d25d16172c0c34
Justin Lim <jlim21@mgh.harvard.edu>
2025-06-24 20:05:56 -0400

    Fiddle with R package installation

---
+++ docker/r/install.R
@@ -0,0 +1,33 @@
+parse_pkgs <- function(x) {
+    strsplit(x, split = "@", fixed = TRUE)
+}
+
+nproc <- function() {
+    withCallingHandlers(as.integer(system2("nproc", stdout = TRUE)),
+                        warning = function(w) 1L)
+}
+
+NCPUS <- nproc()
+
+install <- function(x) {
+    remotes::install_version(
+        x[[1]],
+        version = x[[2]],
+        upgrade = "always",
+        repos = c("https://cloud.r-project.org"),
+        INSTALL_opts = c("--no-docs", "--no-html", "--no-data", "--no-help",
+                         "--no-demo", "--without-keep.source"),
+        Ncpus = NCPUS
+    )
+}
+
+# Packages --------------------------------------------------------------------
+cran_pkgs <- c("data.table@1.17.6",
+               "optparse@1.7.5") |>
+  parse_pkgs()
+bioc_pkgs <- c("Rsamtools@2.24.0",
+               "GenomicRanges@1.60.0") |>
+  parse_pkgs()
+
+invisible(lapply(cran_pkgs, install))
+invisible(lapply(bioc_pkgs, install))

e08a07ae42b3c01adf42a3d04a4cafb1eef7279c
Justin Lim <jlim21@mgh.harvard.edu>
2025-07-24 11:22:02 -0400

    benchmark_denovo: fix not de novo bug
    The script assumes that the test set will have at least one not de novo
    call and crashes if the assumption is violated. We add some checks to
    make sure that the input sets have the data we expect.
    
    Also update naming of "CleanVcf" to reflect the fact that the input VCF
    does not actually have to be from CleanVcf.

--- src/scripts/r/benchmark_denovo.R
+++ src/scripts/r/benchmark_denovo.R
@@ -1,4 +1,4 @@
-# Usage: Rscript benchmark_denovo.R <denovo> <truth> <cleanvcf> <sample_table>
+# Usage: Rscript benchmark_denovo.R <denovo> <truth> <input_vcf> <sample_table>
 
 # if (!require("R.utils", quietly = TRUE)) {
 #     install.packages("R.utils")
@@ -9,7 +9,7 @@ suppressPackageStartupMessages(library(GenomicRanges))
 args <- commandArgs(trailingOnly = TRUE)
 test_path <- args[[1]]
 truth_path <- args[[2]]
-cleanvcf_path <- args[[3]]
+input_vcf_path <- args[[3]]
 sample_table_path <- args[[4]]
 
 svmatch <- function(query, subject, min_ovp, size_similarity, breakend_window) {
@@ -93,94 +93,117 @@ sample_table <- fread(sample_table_path,
                           "other_id",
                           "cohort_short")
 )
+stopifnot("sample table is empty" = nrow(sample_table) > 0)
 setnames(sample_table, "entity:sample_id", "sample_id")
 
 truth <- fread(truth_path, sep = ",")
+stopifnot("truth set is empty" = nrow(truth) > 0)
 setnames(truth, c("#chrom", "pos", "end"), c("chr", "start", "end"))
 truth[, svtype := sub("LINE1|SVA|ALU", "INS", svtype)]
 truth <- truth[(!svtype %in% c("CPX", "CTX", "CNV")) & (mosaic == FALSE), ]
 setnames(truth, "sample", "other_id")
+stopifnot("truth set is empty after filtering" = nrow(truth) > 0)
 
 test <- fread(test_path, sep = "\t")
+stopifnot("test set is empty" = nrow(test) > 0)
 setnames(test, "svtype", "ALT")
 setnames(test, c("chrom", "SVTYPE"), c("chr", "svtype"))
 # Exclude CTX and CPX from comparisons
 test <- test[!svtype %in% c("CPX", "CTX"), ]
+stopifnot("test set is empty after filtering" = nrow(test) > 0)
 test <- merge(test, sample_table, by.x = "sample", by.y = "sample_id", all.x = TRUE)
 
-# bcftools query -i 'GT="alt" & INFO/SVTYPE != "CNV" & INFO/SVTYPE != "BND"' \
-#   -f '%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\t[%SAMPLE,]\n' ssc_quads-annotated.vcf.gz \
-#   | awk -F'\t' '{sub(/,$/, "", $5); split($5, a, /,/); for(i in a){$5=a[i]; print}}' OFS='\t' > clean_vcf_svs.tsv
-cleanvcf <- fread(cleanvcf_path,
+input_vcf <- fread(input_vcf_path,
                   sep = "\t",
                   col.names = c("chr", "start", "end", "svtype", "sample"),
                   header = FALSE)
-cleanvcf <- merge(cleanvcf, sample_table, by.x = "sample", by.y = "sample_id", all.x = TRUE)
+input_vcf <- merge(input_vcf, sample_table, by.x = "sample", by.y = "sample_id", all.x = TRUE)
+stopifnot("input VCF is empty" = nrow(input_vcf) > 0)
 
-# Remove non-SSC from CleanVcf
-cleanvcf <- cleanvcf[cohort_short == "SSC", ]
+# Remove non-SSC samples from input VCF
+input_vcf <- input_vcf[cohort_short == "SSC", ]
+stopifnot("input VCF does not have SSC samples" = nrow(input_vcf) > 0)
 
-cleanvcf_samples <- unique(cleanvcf$other_id)
+input_vcf_samples <- unique(input_vcf$other_id)
 
-# Subset the test set and truth set to SSC samples in CleanVcf
-test <- test[other_id %in% cleanvcf_samples, ]
-message(sprintf("keeping %d / %d calls from truth after matching samples in CleanVcf", sum(truth$other_id %in% cleanvcf_samples), nrow(truth)))
-truth <- truth[other_id %in% cleanvcf_samples, ]
+# Subset the test set and truth set to SSC samples in input VCF
+test <- test[other_id %in% input_vcf_samples, ]
+stopifnot("test set does not have SSC samples" = nrow(test) > 0)
+message(sprintf("keeping %d / %d calls from truth after matching samples in input VCF", sum(truth$other_id %in% input_vcf_samples), nrow(truth)))
+truth <- truth[other_id %in% input_vcf_samples, ]
+stopifnot("truth set does not have SSC sampes" = nrow(truth) > 0)
+
+input_vcf_gr <- dt2gr(input_vcf)
+truth_gr <- dt2gr(truth)
+
+# Which truth calls are in the input VCF --------------------------------------
+truth_in_input_vcf <- overlap(truth_gr, input_vcf_gr)
+stopifnot("no truth SVs found in input VCF" = length(truth_in_input_vcf) > 0)
+message(sprintf("%d / %d truth calls matched in input VCF", length(truth_in_input_vcf), length(truth_gr)))
+
+# Which truth calls are not in input VCF --------------------------------------
+truth_not_in_input_vcf <- setdiff(seq_along(truth_gr), truth_in_input_vcf)
+
+truth_in_input_vcf_dt <- truth[truth_in_input_vcf, ]
+truth_in_input_vcf_gr <- dt2gr(truth_in_input_vcf_dt)
 
 dn <- test[is_de_novo == TRUE, ]
 not_dn <- test[is_de_novo == FALSE, ]
 
-dn_gr <- dt2gr(dn)
-not_dn_gr <- dt2gr(not_dn)
-cleanvcf_gr <- dt2gr(cleanvcf)
-truth_gr <- dt2gr(truth)
+if (nrow(dn) > 0) {
+    dn_gr <- dt2gr(dn)
+    # PPV (positive predictive value): which de novo calls are in truth
+    # true positive / all positive
+    dn_in_truth <- overlap(dn_gr, truth_in_input_vcf_gr)
+    message(sprintf("PPV: %d / %d = %0.3f", length(dn_in_truth), length(dn_gr), length(dn_in_truth) / length(dn_gr)))
 
-# Which truth calls are in CleanVcf -------------------------------------------
-truth_in_cleanvcf <- overlap(truth_gr, cleanvcf_gr)
-message(sprintf("%d / %d truth calls matched in CleanVcf", length(truth_in_cleanvcf), length(truth_gr)))
+    # FDR (false discovery rate): which de novo calls are not in truth
+    # false positive / all positive
+    dn_not_in_truth <- setdiff(seq_along(dn_gr), dn_in_truth)
+    message(sprintf("FDR: %d / %d = %0.3f", length(dn_not_in_truth), length(dn_gr), length(dn_not_in_truth) / length(dn_gr)))
 
-# Which truth calls are not in CleanVcf ---------------------------------------
-truth_not_in_cleanvcf <- setdiff(seq_along(truth_gr), truth_in_cleanvcf)
+    # sensitivity: which truth calls are in de novo calls
+    # captured positive / true positives
+    truth_in_dn <- overlap(truth_in_input_vcf_gr, dn_gr)
+    message(sprintf("sensitivity: %d / %d = %0.3f", length(truth_in_dn), length(truth_in_input_vcf_gr), length(truth_in_dn) / length(truth_in_input_vcf_gr)))
 
-truth_in_cleanvcf_dt <- truth[truth_in_cleanvcf, ]
-truth_in_cleanvcf_gr <- dt2gr(truth_in_cleanvcf_dt)
+    # missed calls: which truth calls are not in de novo calls
+    truth_not_in_dn <- setdiff(seq_along(truth_in_input_vcf_gr), truth_in_dn)
+} else {
+    message("no de novo calls in test set")
+    message("skipping PPV, FDR, and sensitivity")
+    dn_in_truth <- integer(0)
+    dn_not_in_truth <- integer(0)
+    truth_in_dn <- integer(0)
+    truth_not_in_dn <- seq_along(truth_in_input_vcf_gr)
+}
 
-# PPV (positive predictive value): which de novo calls are in truth -----------
-# true positive / all positive
-dn_in_truth <- overlap(dn_gr, truth_in_cleanvcf_gr)
-message(sprintf("PPV: %d / %d = %0.3f", length(dn_in_truth), length(dn_gr), length(dn_in_truth) / length(dn_gr)))
+if (nrow(not_dn) > 0) {
+    not_dn_gr <- dt2gr(not_dn)
+    # FOR (false omission rate): which not de novo calls are in truth
+    # false negative / all negative
+    not_dn_in_truth <- overlap(not_dn_gr, truth_in_input_vcf_gr)
+    message(sprintf("FOR: %d / %d = %0.3f", length(not_dn_in_truth), length(not_dn_gr), length(not_dn_in_truth) / length(not_dn_gr)))
 
-# FDR (false discovery rate): which de novo calls are not in truth ------------
-# false positive / all positive
-dn_not_in_truth <- setdiff(seq_along(dn_gr), dn_in_truth)
-message(sprintf("FDR: %d / %d = %0.3f", length(dn_not_in_truth), length(dn_gr), length(dn_not_in_truth) / length(dn_gr)))
+    # NPV (negative predictive value): which not de novo calls are not in truth
+    # true negative / all negative
+    not_dn_not_in_truth <- setdiff(seq_along(not_dn_gr), not_dn_in_truth)
+    message(sprintf("NPV: %d / %d = %0.3f", length(not_dn_not_in_truth), length(not_dn_gr), length(not_dn_not_in_truth) / length(not_dn_gr)))
+} else {
+    message("no not de novo calls in the test set")
+    message("skipping FOR and NPV")
+    not_dn_in_truth <- integer(0)
+    not_dn_not_in_truth <- integer(0)
+}
 
-# FOR (false omission rate): which not de novo calls are in truth -------------
-# false negative / all negative
-not_dn_in_truth <- overlap(not_dn_gr, truth_in_cleanvcf_gr)
-message(sprintf("FOR: %d / %d = %0.3f", length(not_dn_in_truth), length(not_dn_gr), length(not_dn_in_truth) / length(not_dn_gr)))
-
-# NPV (negative predictive value): which not de novo calls are not in truth ---
-# true negative / all negative
-not_dn_not_in_truth <- setdiff(seq_along(not_dn_gr), not_dn_in_truth)
-message(sprintf("NPV: %d / %d = %0.3f", length(not_dn_not_in_truth), length(not_dn_gr), length(not_dn_not_in_truth) / length(not_dn_gr)))
-
-# sensitivity: which truth calls are in de novo calls -------------------------
-# captured positive / true positives
-truth_in_dn <- overlap(truth_in_cleanvcf_gr, dn_gr)
-message(sprintf("sensitivity: %d / %d = %0.3f", length(truth_in_dn), length(truth_in_cleanvcf_gr), length(truth_in_dn) / length(truth_in_cleanvcf_gr)))
-
-# missed calls: which truth calls are not in de novo calls --------------------
-truth_not_in_dn <- setdiff(seq_along(truth_in_cleanvcf_gr), truth_in_dn)
-
-fwrite(truth[truth_in_cleanvcf, ], "truth_in_cleanvcf.tsv", sep = "\t")
-fwrite(truth[truth_not_in_cleanvcf, ], "truth_not_in_cleanvcf.tsv", sep = "\t")
+fwrite(truth[truth_in_input_vcf, ], "truth_in_input_vcf.tsv", sep = "\t")
+fwrite(truth[truth_not_in_input_vcf, ], "truth_not_in_input_vcf.tsv", sep = "\t")
 fwrite(dn[dn_in_truth, ], "denovo_in_truth.tsv", sep = "\t")
 fwrite(dn[dn_not_in_truth, ], "denovo_not_in_truth.tsv", sep = "\t")
 fwrite(not_dn[not_dn_in_truth, ], "not_denovo_in_truth.tsv", sep = "\t")
 fwrite(not_dn[not_dn_not_in_truth, ], "not_denovo_not_in_truth.tsv", sep = "\t")
-fwrite(truth_in_cleanvcf_dt[truth_in_dn, ], "truth_in_denovo.tsv", sep = "\t")
-fwrite(truth_in_cleanvcf_dt[truth_not_in_dn, ], "truth_not_in_denovo.tsv", sep = "\t")
+fwrite(truth_in_input_vcf_dt[truth_in_dn, ], "truth_in_denovo.tsv", sep = "\t")
+fwrite(truth_in_input_vcf_dt[truth_not_in_dn, ], "truth_not_in_denovo.tsv", sep = "\t")
 
 old_par <- par(no.readonly = TRUE)
 jpeg("benchmark_venn.jpeg", width = 800, height = 450)
@@ -192,7 +215,7 @@ symbols(0.7, 0.4, circles = 0.35, inches = FALSE, add = TRUE, bg = "#CBE11E66")
 text(0.35, 0.1, labels = "input VCF", cex = 1, adj = 0.5)
 text(0.9, 0.5, labels = "Belyeu", cex = 1, adj = 0.5)
 text(0.35, 0.85, labels = "de novo pipeline", cex = 1, adj = 0.5)
-text(0.9, 0.4, labels = length(truth_not_in_cleanvcf), cex = 2, adj = 0.5)
+text(0.9, 0.4, labels = length(truth_not_in_input_vcf), cex = 2, adj = 0.5)
 text(0.55, 0.2, labels = length(truth_not_in_dn), cex = 2, adj = 0.5)
 text(0.5, 0.5, labels = length(dn_in_truth), cex = 2, adj = 0.5)
 text(0.3, 0.7, labels = length(dn_not_in_truth), cex = 2, adj = 0.5)

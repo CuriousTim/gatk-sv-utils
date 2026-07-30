c07427511ea1255cc6ddfafd6307828f89700d9f
Justin Lim <jlim21@mgh.harvard.edu>
2026-07-30 16:14:36 -0400

    free the gorilla

--- src/gorilla/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    env <- topenv()
-    env$gc_token <- gargle::token_fetch(
-        "https://www.googleapis.com/auth/cloud-platform"
-    )
-}
--- src/gorilla/tests/testthat/test-is_string.R
+++
@@ -1,3 +0,0 @@
-test_that("`is_string` works", {
-    expect_true(is_string("nawr"))
-})
